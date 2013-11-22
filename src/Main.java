1291bc1ceed434744fb308ffb7a899356c545040
Thomas Coe <thomascoe1@gmail.com>
2013-11-22 11:59:05 -0500

    Displayed time for benchmarking the time to sort

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
  */
 public class Main {
     public static void main(String[] args) {
+        long time = 0L;
         //Original array setup
         int[] array = {64,25,12,22,11};
         System.out.print("Original Array: ");
@@ -15,26 +16,34 @@ public class Main {
         //Bubble Sort
         System.out.print("Test BubbleSort: ");
         int[] array2 = array.clone();
+        time = System.nanoTime();
         IntSort.bubbleSort(array2);
         IntSort.printArr(array2);
+        System.out.println("Time: " + (System.nanoTime() - time));
         System.out.println();
         //Selection Sort
         System.out.print("Test SelectionSort: ");
         int[] array3 = array.clone();
+        time = System.nanoTime();
         IntSort.selectionSort(array3);
         IntSort.printArr(array3);
+        System.out.println("Time: " + (System.nanoTime() - time));
         System.out.println();
         //Insertion Sort
         System.out.print("Test InsertionSort: ");
         int[] array4 = array.clone();
+        time = System.nanoTime();
         IntSort.insertionSort(array4);
         IntSort.printArr(array4);
+        System.out.println("Time: " + (System.nanoTime() - time));
         System.out.println();
         //Bogo Sort
         System.out.print("Test BogoSort: ");
         int[] array5 = array.clone();
+        time = System.nanoTime();
         IntSort.bogoSort(array5);
         IntSort.printArr(array5);
+        System.out.println("Time: " + (System.nanoTime() - time));
         System.out.println();
     }
 }
